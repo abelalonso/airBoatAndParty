2803f65c4908d76984e81a4578f0ffc00116bf15
Abel Alonso <abel.alonso@gmail.com>
2018-07-31 00:18:16 +0200

    rutas en el back ok

---
+++ server/api/auth/User.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {type: String, required: true},
+  password: {type: String, required: true},
+  role: { 
+    type: String, 
+    enum: ["admin", "user", "owner"],
+    default: "user"
+  },
+  email: {type: String, required: true},
+  name: {type: String, required: true},
+  surname1: {type: String, required: true},
+  surname2: String,
+  boats: {type: [Schema.Types.ObjectId], ref: 'Boat'},
+  profileImage: String
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
---
+++ server/api/auth/auth-routes.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const authRouter  = express.Router();
+const User = require('./User.model');
+const passport = require('passport');
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+//Auto-login
+const login = (req, user) => {
+  return new Promise((resolve,reject) => {
+    req.login(user, err => {
+      if(err) {
+        reject(new Error('Something went wrong'))
+      }else{
+        resolve(user);
+      }
+    })
+  })
+}
+
+// SIGNUP
+authRouter.post("/signup", (req, res, next) => {
+  const {username, password, name, surname1, surname2, email, profileImage} = req.body;
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username })
+    .then( foundUser => {
+    if (foundUser !== null) throw new Error('Username already exists');
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    return newUser = new User({
+      username,
+      password: hashPass,
+      name, 
+      surname1, 
+      surname2, 
+      email, 
+      profileImage,
+      boats: []
+    }).save()
+    .then( savedUser => login(req, savedUser)) // Login the user using passport
+    .then( user => res.json({status: 'signup & login successfully', user})) // Answer JSON
+    .catch(e => next(e));
+  })
+  .catch(e => next(e));
+});
+
+
+authRouter.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, theUser, failureDetails) => {
+    
+    // Check for errors
+    if (err) next(new Error('Something went wrong')); 
+    if (!theUser) next(failureDetails)
+
+    // Return user and logged in
+    login(req, theUser).then(user => res.status(200).json(req.user));
+
+  })(req, res, next);
+});
+
+
+authRouter.get('/currentuser', (req,res,next) => {
+  if(req.user){
+    res.status(200).json(req.user);
+  }else{
+    next(new Error('Not logged in'))
+  }
+})
+
+
+authRouter.get('/logout', (req,res) => {
+  req.logout();
+  res.status(200).json({message:'logged out'})
+});
+
+
+authRouter.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+})
+
+module.exports = authRouter;
