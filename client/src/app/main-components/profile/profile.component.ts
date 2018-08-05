52d26e57d72413e5594057d6beb771029abf7356
Abel Alonso <abel.alonso@gmail.com>
2018-08-06 00:13:42 +0200

    Update de la foto del user

--- client/src/app/main-components/profile/profile.component.ts
+++ client/src/app/main-components/profile/profile.component.ts
@@ -3,6 +3,10 @@ import { SessionService } from '../../session/session.service';
 import { BoatService } from '../boat.service';
 import { Boat } from '../boat-interface';
 import { Router } from '@angular/router';
+import { FileUploader } from 'ng2-file-upload';
+import { environment } from '../../../environments/environment';
+
+const { BASEURL } = environment;
 
 @Component({
   selector: 'app-profile',
@@ -14,6 +18,14 @@ export class ProfileComponent implements OnInit {
   boats: Array<Boat>
   emailForm = false;
   nameForm = false;
+  imageForm = false;
+  userId;
+
+  uploader: FileUploader = new FileUploader ({
+    url: `${BASEURL}/api/auth/update/${this.sessionService.user._id}`,
+    method: 'PATCH'
+  });
+  feedback;
 
   constructor(private sessionService: SessionService, private boatService: BoatService, private router: Router) {
 
@@ -21,6 +33,15 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit() {
 
+    this.uploader.onSuccessItem = (item, response) => {
+      this.feedback = JSON.parse(response).message;
+      this.sessionService.isLogged().subscribe();
+    };
+
+    this.uploader.onErrorItem = (item, response, status, headers) => {
+      this.feedback = JSON.parse(response).message;
+    };
+    
   }
   update(){
     this.boatService.getBoats("").subscribe( boats => this.boats=boats);
@@ -36,6 +57,14 @@ export class ProfileComponent implements OnInit {
     this.sessionService.update({name, surname}).subscribe(()=>{
       this.nameForm=false;
     })
+  }
+
+  updateImage(){
+    this.uploader.uploadAll();
+ //     this.uploader.onCompleteItem = () => {
+        this.imageForm = false;
+
+   //   };
   }
 
 }
